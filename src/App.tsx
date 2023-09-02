e2a4629f9380117f7b9e506d929d6869bad07e1f
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-02 17:48:46 +0530

    Clean the setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container pt-3'>
+      <div className='row'>
+        <div className='col-lg-12'>
+          <h1 className='h3 text-center'>App Works</h1>
+        </div>
+      </div>
     </div>
   );
 }
